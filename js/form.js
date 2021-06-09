128a9251f308d68e56333786c1eecd0a79435aae
aziz <azizsassu2015@gmail.com>
2021-06-09 21:16:28 +0500

    end form

--- js/form.js
+++ js/form.js
@@ -8,9 +8,11 @@ errorText = document.querySelector('.error');
 // отключил обновление странички при сабмите если импут пустой
 form.addEventListener('submit', (e)=>{
     let messages = [];
-    if(user.value === '' || user.value == null) messages.push('Требован имя') 
-    if(messages.length > 0){
+    if(user.value === '' || user.value == null) messages.push('Введи имя чмо блять')
+    else if(mail.value === '' || user.value == null || user.value === !NaN ) messages.push("Введи адрес чмо блять") 
+    if( messages.length > 0){
         e.preventDefault()
+        user.classList.add = 'red'
         errorText.innerText = messages.join(', ')
     }
 })
